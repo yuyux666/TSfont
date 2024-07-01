0bb16d757e12a4320f45ed4ec655592df9f435e2
fff <3032351083@qq.com>
2024-07-01 13:18:13 +0800

    投诉提交查看，查看酒店订单

--- src/views/TouristComplaints/components/ComplaintCheck.vue
+++ src/views/TouristComplaints/components/ComplaintCheck.vue
@@ -1,7 +1,7 @@
 <script setup>
 import ScenePanel from '@/views/Common/ScenePanel.vue'
 import EvaluateItem from '../extended/EvaluateItem.vue'
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import { complaintViewService } from '@/apis/complaint'
 
 const evaluateRef = ref()
@@ -9,7 +9,8 @@ const total = ref(0)
 // 定义请求参数对象
 const params = ref({
   currPage: 1,
-  size: 5
+  size: 5,
+  total
 })
 
 const contentForm = ref({
@@ -25,63 +26,82 @@ const showEvaluationDialog = (row) => {
   evaluateRef.value.open(row)
 }
 
-const complaintList = [
-  {
-    id: '1',
-    submit_time: '2021-03-12', // 投诉时间
-    complainter_name: '李投诉', // 投诉人
-    title: '强制消费！', // 投诉标题
-    result_content: '已处理', // 处理结果
-    processing_time: '2021-03-14', // 处理时间
-    processor_name: '王处理', // 处理人
-    approvere_name: '张审批', // 审批人
-    status: '处理完成', // 投诉状态
-    grade: '' // 评分
-  },
-  {
-    id: '2',
-    submit_time: '2021-03-12', // 投诉时间
-    complainter_name: '李投诉', // 投诉人
-    title: '厕所差', // 投诉标题
-    result_content: '已处理', // 处理结果
-    processing_time: '2021-03-14', // 处理时间
-    processor_name: '王处理', // 处理人
-    approvere_name: '张审批', // 审批人
-    status: '处理完成', // 投诉状态
-    grade: '3' // 评分
-  },
-  {
-    id: '3',
-    submit_time: '2021-03-12', // 投诉时间
-    complainter_name: '李投诉', // 投诉人
-    title: '垃圾多', // 投诉标题
-    result_content: '', // 处理结果
-    processing_time: '2021-03-14', // 处理时间
-    processor_name: '王处理', // 处理人
-    approvere_name: '张审批', // 审批人
-    status: '待处理', // 投诉状态
-    grade: '' // 评分
-  },
-  {
-    id: '',
-    submit_time: '2021-03-12', // 投诉时间
-    complainter_name: '李投诉', // 投诉人
-    title: '垃圾多', // 投诉标题
-    result_content: '', // 处理结果
-    processing_time: '2021-03-14', // 处理时间
-    processor_name: '王处理', // 处理人
-    approvere_name: '张审批', // 审批人
-    status: '待处理', // 投诉状态
-    grade: '' // 评分
-  }
-]
-
+// const complaintList = [
+//   {
+//     id: '1',
+//     submit_time: '2021-03-12', // 投诉时间
+//     complainter_name: '李投诉', // 投诉人
+//     title: '强制消费！', // 投诉标题
+//     result_content: '已处理', // 处理结果
+//     processing_time: '2021-03-14', // 处理时间
+//     processor_name: '王处理', // 处理人
+//     approvere_name: '张审批', // 审批人
+//     status: '处理完成', // 投诉状态
+//     grade: '' // 评分
+//   },
+//   {
+//     id: '2',
+//     submit_time: '2021-03-12', // 投诉时间
+//     complainter_name: '李投诉', // 投诉人
+//     title: '厕所差', // 投诉标题
+//     result_content: '已处理', // 处理结果
+//     processing_time: '2021-03-14', // 处理时间
+//     processor_name: '王处理', // 处理人
+//     approvere_name: '张审批', // 审批人
+//     status: '处理完成', // 投诉状态
+//     grade: '3' // 评分
+//   },
+//   {
+//     id: '3',
+//     submit_time: '2021-03-12', // 投诉时间
+//     complainter_name: '李投诉', // 投诉人
+//     title: '垃圾多', // 投诉标题
+//     result_content: '', // 处理结果
+//     processing_time: '2021-03-14', // 处理时间
+//     processor_name: '王处理', // 处理人
+//     approvere_name: '张审批', // 审批人
+//     status: '待处理', // 投诉状态
+//     grade: '' // 评分
+//   },
+//   {
+//     id: '',
+//     submit_time: '2021-03-12', // 投诉时间
+//     complainter_name: '李投诉', // 投诉人
+//     title: '垃圾多', // 投诉标题
+//     result_content: '', // 处理结果
+//     processing_time: '2021-03-14', // 处理时间
+//     processor_name: '王处理', // 处理人
+//     approvere_name: '张审批', // 审批人
+//     status: '待处理', // 投诉状态
+//     grade: '' // 评分
+//   }
+// ]
+const complaintList = ref([])
 const getComplaintList = async () => {
   const res = await complaintViewService(params.value)
   console.log(res)
   complaintList.value = res.data.data.records
+  params.value.total = res.data.data.total
+  console.log(res.data.data)
 }
-getComplaintList()
+onMounted(() => {
+  getComplaintList()
+})
+const onCurrentChange = () => {
+  getComplaintList()
+}
+const onSizeChange = () => {
+  getComplaintList()
+}
+const diglogContent = ref({
+  title: '',
+  content: ''
+})
+const showComplaintContent = (title, content) => {
+  complaintDetailVisible.value = true
+  diglogContent.value.title = title
+  diglogContent.value.content = content
+}
 </script>
 <template>
   <div class="mycontainer">
@@ -92,17 +112,17 @@ getComplaintList()
         v-loading="loading"
         style="width: 90; border-radius: 10px; min-height: 300px"
       >
-        <el-table-column label="投诉日期" prop="submitTime"></el-table-column>
-        <el-table-column
-          label="投诉人"
-          prop="complainter_name"
-        ></el-table-column>
+        <el-table-column label="投诉日期" prop="submitTime">
+          <template #default="{ row }">
+            {{ row.submitTime.slice(0, 10) }}
+          </template>
+        </el-table-column>
         <el-table-column label="投诉标题" prop="title">
           <!-- 会以默认插槽为主，prop可以被忽略 -->
           <template #default="{ row }">
             <el-link
               :underline="false"
-              @click="complaintDetailVisible = true"
+              @click="showComplaintContent(row.title, row.content)"
               >{{ row.title }}</el-link
             >
           </template>
@@ -112,29 +132,9 @@ getComplaintList()
           label="处理时间"
           prop="processingTime"
         ></el-table-column>
-        <el-table-column label="处理员" prop="processor_name"></el-table-column>
-        <el-table-column label="审核员" prop="approvere_name"></el-table-column>
+        <el-table-column label="处理员" prop="processors"></el-table-column>
+        <el-table-column label="审核员" prop="approver"></el-table-column>
         <el-table-column label="投诉状态" prop="status">
-          <!-- 利用作用域插槽row，可以获取当前行的数据 -->
-          <!-- 相当于v-for的item -->
-          <!-- <template #default="{ row }"
-            ><el-button
-              circle
-              plain
-              type="primary"
-              :icon="Edit"
-              @click="onEditArticle(row)"
-            ></el-button>
-            <el-button
-              circle
-              plain
-              type="danger"
-              :icon="Delete"
-              @click="onDeleteArticle(row)"
-            ></el-button
-          ></template> -->
-        </el-table-column>
-        <el-table-column label="投诉评价" prop="grade">
           <template #default="{ row }">
             <template v-if="row.status === '处理完成'">
               <el-rate
@@ -152,15 +152,16 @@ getComplaintList()
             </template>
           </template>
         </el-table-column>
+        <el-table-column label="投诉评价" prop="grade"> </el-table-column>
       </el-table>
       <!-- 分页 -->
       <el-pagination
         v-model:current-page="params.currPage"
         v-model:page-size="params.size"
-        :page-sizes="[2, 3, 5, 10]"
+        :page-sizes="[5, 10]"
         :background="true"
         layout="jumper, total, sizes, prev, pager, next"
-        :total="total"
+        :total="params.total"
         @size-change="onSizeChange"
         @current-change="onCurrentChange"
         style="margin-top: 50px; justify-content: flex-end"
@@ -193,13 +194,13 @@ getComplaintList()
         </el-form-item>
         <el-form-item label="投诉标题" prop="title">
           <el-input
-            v-model="contentForm.title"
+            v-model="diglogContent.title"
             placeholder="请输入标题"
             disabled
           ></el-input>
         </el-form-item>
         <el-form-item label="投诉内容" prop="content">
-          <el-input v-model="contentForm.content" type="textarea" disabled />
+          <el-input v-model="diglogContent.content" type="textarea" disabled />
         </el-form-item>
         <el-form-item label="附件列表" prop="file_name">
           <span>{{ contentForm.file_name }}</span>
